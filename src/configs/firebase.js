1b89f8f3491588545dab3d35f099ab3ca77cd8d7
Lucas Oliveira <luaks.skt8@gmail.com>
2022-01-18 05:04:56 -0300

    remove some comments

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -3,16 +3,6 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 import { getAuth } from 'firebase/auth';
 
-// const firebaseConfig = {
-//   apiKey: 'AIzaSyBkWQ4j2l7MbyKyDWsYV7mkFaiyl6kukJE',
-//   authDomain: 'my-awesome-tasks-9dfa0.firebaseapp.com',
-//   databaseURL: 'https://my-awesome-tasks-9dfa0-default-rtdb.firebaseio.com',
-//   projectId: 'my-awesome-tasks-9dfa0',
-//   storageBucket: 'my-awesome-tasks-9dfa0.appspot.com',
-//   messagingSenderId: '306480389595',
-//   appId: '1:306480389595:web:cbc32efbe6acd93a7edbfe',
-// };
-
 const firebaseConfig = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
